abc6b106405486bd3e03b5aaded0898192e8135c
splohms <splohmar@me.com>
2022-11-10 15:15:02 -0500

    current location

--- map.js
+++ map.js
@@ -45,9 +45,11 @@ JF.getFormSubmissions("223124226628047", function (response) {
   const map = new mapboxgl.Map({
     container: document.body,
     style: "mapbox://styles/splohmar/cl9wu1yx3002q15s5ndaa0sms", // Your style URL
-    center: [-71.10326, 42.36476], // starting position [lng, lat]
-    zoom: 12, // starting zoom
+    center: [-71.093772,42.358839], // starting position [lng, lat]
+    zoom: 16, // starting zoom
     projection: "globe", // display the map as a 3D globe
+    pitch: 60, // pitch in degrees
+    bearing: -60, // bearing in degrees
   });
 
   map.on("load", () => {
@@ -86,7 +88,11 @@ JF.getFormSubmissions("223124226628047", function (response) {
 
       }),
       firstLabelLayerId
+
+
     );
+
+
     function getImageGallery(images, text) {
       const imageGallery = document.createElement("div");
       imageGallery.id = "image-gallery";
@@ -150,6 +156,78 @@ JF.getFormSubmissions("223124226628047", function (response) {
         essential: true, // this animation is considered essential with respect to prefers-reduced-motion
       });
     }
+
+    // create “current location” function, which doesn’t trigger until called upon.
+    function addUserLocation(latitude, longitude) {
+      return map.addLayer(
+        new MapboxLayer({
+          id: "user-location",
+          type: ScatterplotLayer,
+          data: [{ longitude, latitude }],
+          getPosition: (d) => [d.longitude, d.latitude],
+          getSourceColor: [0, 255, 0],
+          sizeScale: 15,
+          getSize: 10,
+          radiusUnits: "pixels",
+          getRadius: 5,
+          opacity: 0.7,
+          stroked: false,
+          filled: true,
+          radiusScale: 3,
+          getFillColor: [3, 202, 252],
+          parameters: {
+            depthTest: false,
+          },
+        })
+      );
+    }
+
+    // get current location
+    const successCallback = (position) => {
+      // add new point layer of current location to deck gl
+      const { latitude, longitude } = position.coords;
+      addUserLocation(latitude, longitude);
+    };
+
+    const errorCallback = (error) => {
+      console.log(error);
+    };
+
+// create async function to await for current location and then return the promise as lat long coordinates then resolve the promise
+    function getCurrentLocation() {
+      const currentLocation = navigator.geolocation.getCurrentPosition(
+        successCallback,
+        errorCallback
+      );
+      return currentLocation;
+    }
+    if (navigator.geolocation) {
+      getCurrentLocation();
+    }
+    const locationButton = document.createElement("div");
+    // create a button that will request the users location
+    locationButton.textContent = "Where am I?";
+    locationButton.id = "location-button";
+    locationButton.addEventListener("click", () => {
+      // when clicked, get the users location
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
+
+          locationButton.textContent =
+            "Where am I? " +
+            position.coords.latitude.toFixed(3) +
+            ", " +
+            position.coords.longitude.toFixed(3);
+
+          addUserLocation(latitude, longitude);
+          flyToClick([longitude, latitude]);
+        });
+      }
+    });
+
+    // append the button
+    document.body.appendChild(locationButton);
 
   });
 });
